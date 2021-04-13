323a306a7281680eb6098848b2e83907476d1ee9
JongAh_Gang <80739992+jongah@users.noreply.github.com>
2021-04-13 18:17:47 +0900

    Add files via upload

---
+++ in_class/AOP/AdviceClass.java
@@ -0,0 +1,10 @@
+package kr.hs.study.advisor;
+
+public class AdviceClass {
+	public void before() {
+		System.out.println("before 호출됨");
+	}
+	public void after() {
+		System.out.println("after 호출됨");
+	}
+}
---
+++ in_class/AOP/TestBean1.java
@@ -0,0 +1,16 @@
+package kr.hs.study.beans;
+
+public class TestBean1 {
+	public void method1() {
+		System.out.println("method1");
+	}
+	public void method2() {
+		System.out.println("method2");
+	}
+	public void func1() {
+		System.out.println("func1");
+	}
+	public void func2() {
+		System.out.println("func2");
+	}
+}
